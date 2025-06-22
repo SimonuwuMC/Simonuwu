c3df800f9be981ed3c3a8befb880181b8ab1812a
SimonuwuMC <desousasimon023@gmail.com>
2025-06-22 07:33:29 -0400

    Updated DownloadSection.js

--- src/components/DownloadSection.js
+++ src/components/DownloadSection.js
@@ -15,7 +15,7 @@ const DownloadSection = ({ onAchievement }) => {
     '1.21.4': 'https://images.pexels.com/photos/1670977/pexels-photo-1670977.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080',
     '1.21.3': 'https://images.pexels.com/photos/1670977/pexels-photo-1670977.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080',
     '1.21.1': 'https://images.pexels.com/photos/1670977/pexels-photo-1670977.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080',
-    '1.20.4': 'https://images.pexels.com/photos/1670977/pexels-photo-1670977.jpeg?auto=compress&cs=tinysrgb&w=1920&h=1080',
+    '1.20.4': 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Vanilla-PMP_Collection-Carousel-0_Trails-and-Tales_1280x768.jpg',
     '1.18.2': 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/CC2_KeyArt_1140x565.jpg'
   };
 
