b67fc1191c79c75324d9459947011f9ccf486e9a
SimonuwuMC <desousasimon023@gmail.com>
2025-07-01 16:53:14 -0400

    Agregar nueva versión beta 0.3 para Minecraft 1.21.6

--- src/data/versions/minecraft-1.21.6.js
+++ src/data/versions/minecraft-1.21.6.js
@@ -4,6 +4,14 @@ export const minecraft1216 = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/MCV_ChaseTheSkies_launch_Header_1170x500.jpg',
   releases: [
     {
+      id: "90fV8Rth",
+      version: "beta-0.3-1.0.4",
+      date: "Jun 26, 2025",
+      changelog: "Nueva versión beta 0.3 con mejoras adicionales y correcciones de errores para Minecraft 1.21.6",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.3-1.0.4-1.21.6",
+      isBeta: true
+    },
+    {
       id: "80dU7Qsg",
       version: "beta-0.2-1.0.4",
       date: "Jun 24, 2025",
