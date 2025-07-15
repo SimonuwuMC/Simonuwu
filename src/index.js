a02d2e5f84fa323a23f9f42e43b295aabc9cb980
SimonuwuMC <desousasimon023@gmail.com>
2025-07-15 16:54:51 -0400

    Fix SyntaxError by consolidating into single bundle

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import PojavApp from './PojavApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Check if we're on the pojav.html page
+const isPojavPage = window.location.pathname === '/pojav.html';
+
 root.render(
   <React.StrictMode>
-    <App />
+    {isPojavPage ? <PojavApp /> : <App />}
   </React.StrictMode>
 );
