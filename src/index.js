224cdeb0ef16b9be70ad2bfd2706ef4d72d58e05
SimonuwuMC <desousasimon023@gmail.com>
2025-07-19 12:26:10 -0400

    Remove all PojavApp references from src/index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import PojavApp from './PojavApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Check if we're on the pojav.html page
-const isPojavPage = window.location.pathname === '/pojav.html';
-
 root.render(
   <React.StrictMode>
-    {isPojavPage ? <PojavApp /> : <App />}
+    <App />
   </React.StrictMode>
 );
