d85435f5601b1b2c67bbdfac243653074ce38674
SimonuwuMC <desousasimon023@gmail.com>
2025-07-24 18:15:28 -0400

    Updated CompatibilityModal.js

--- src/components/CompatibilityModal.js
+++ src/components/CompatibilityModal.js
@@ -24,7 +24,7 @@ const CompatibilityModal = ({ isOpen, onClose }) => {
             {/* Display the table image directly */}
             <div className="w-full h-full overflow-auto">
               <img 
-                src="/Captura de pantalla 2025-07-19 123016.png" 
+                src="/Captura de pantalla 2025-07-24 181155.png" 
                 alt="Tabla de Compatibilidad de Mods"
                 className="w-full h-auto min-w-[800px] object-contain"
                 style={{ imageRendering: 'crisp-edges' }}
