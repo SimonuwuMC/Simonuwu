a0b40fe5919d803ad26c28dae68433cf5976a2ba
SimonuwuMC <desousasimon023@gmail.com>
2025-08-24 06:17:52 -0400

    Updated firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,16 +2,7 @@
  import { getFirestore } from 'firebase/firestore';
  import { getAnalytics } from 'firebase/analytics';
  
--// TODO: Replace with your Firebase config
--// For now, using demo mode - check console for warnings
- const firebaseConfig = {
--  apiKey: "demo-key",
--  authDomain: "demo-project.firebaseapp.com",
--  projectId: "demo-project",
--  storageBucket: "demo-project.appspot.com",
--  messagingSenderId: "123456789",
--  appId: "1:123456789:web:demo",
--  measurementId: "G-DEMO"
+
 +  apiKey: "AIzaSyD1fyZ19Wt_qSdHvxTgJsgJxFK3_grFMJ0",
 +  authDomain: "simonuwu-modpack.firebaseapp.com",
 +  projectId: "simonuwu-modpack",
@@ -26,8 +17,5 @@
  export const auth = getAuth(app);
  export const db = getFirestore(app);
  export const analytics = getAnalytics(app);
--
--// Demo mode warning
--console.warn('🔧 Firebase running in DEMO mode. Configure real credentials in firebase.js');
--
+
  export default app;
