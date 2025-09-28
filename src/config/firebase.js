3ce9fdc8d5e692b700b38f89d24b012343d6ceb4
SimonuwuMC <desousasimon023@gmail.com>
2025-09-28 18:29:51 -0400

    Fix Firebase configuration syntax error

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,24 @@
-@@ .. @@
- import { getFirestore } from 'firebase/firestore';
- import { getAnalytics } from 'firebase/analytics';
- 
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getAnalytics } from 'firebase/analytics';
 
-+  apiKey: "AIzaSyD1fyZ19Wt_qSdHvxTgJsgJxFK3_grFMJ0",
-+  authDomain: "simonuwu-modpack.firebaseapp.com",
-+  projectId: "simonuwu-modpack",
-+  storageBucket: "simonuwu-modpack.firebasestorage.app",
-+  messagingSenderId: "277357670010",
-+  appId: "1:277357670010:web:e35d9e59c1b4bead0bd5c3",
-+  measurementId: "G-Q4S480F1D9"
- };
- 
- // Initialize Firebase
-@@ .. @@
- export const auth = getAuth(app);
- export const db = getFirestore(app);
- export const analytics = getAnalytics(app);
+const firebaseConfig = {
+  apiKey: "AIzaSyD1fyZ19Wt_qSdHvxTgJsgJxFK3_grFMJ0",
+  authDomain: "simonuwu-modpack.firebaseapp.com",
+  projectId: "simonuwu-modpack",
+  storageBucket: "simonuwu-modpack.firebasestorage.app",
+  messagingSenderId: "277357670010",
+  appId: "1:277357670010:web:e35d9e59c1b4bead0bd5c3",
+  measurementId: "G-Q4S480F1D9"
+};
 
- export default app;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
+
+export default app;
