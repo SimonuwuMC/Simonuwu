fabdcda7c496f15a18305b6b6da21cd0bf84d2cf
SimonuwuMC <desousasimon023@gmail.com>
2025-10-05 08:18:42 -0400

    Agregar versión beta 0.2 para Minecraft 1.21.9

--- src/data/versions/minecraft-1.21.9.js
+++ src/data/versions/minecraft-1.21.9.js
@@ -4,6 +4,14 @@ export const minecraft1219 = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Minecraft_Fall_Drop_Campaign_Key_Art_DotNet_Blog_Editorial_1280x768.jpg',
   releases: [
     {
+      id: "160hY6Zwn",
+      version: "beta-0.2-1.0.4",
+      date: "Dec 20, 2025",
+      changelog: "Segunda versión beta para Minecraft 1.21.9 Copper Age con más mods agregados y mejoras de rendimiento",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.2-1.0.4-1.21.9",
+      isBeta: true
+    },
+    {
       id: "150hY5Zwm",
       version: "beta-0.1-1.0.4",
       date: "Dec 15, 2025",
