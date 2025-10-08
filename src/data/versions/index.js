c46864034366eeeae2896f36ea813bc4e1a09ccf
SimonuwuMC <desousasimon023@gmail.com>
2025-10-08 16:22:31 -0400

    Agregar Minecraft 1.21.10

--- src/data/versions/index.js
+++ src/data/versions/index.js
@@ -12,9 +12,11 @@ import { minecraft1194 } from './minecraft-1.19.4';
 import { minecraft1182 } from './minecraft-1.18.2';
 import { minecraft1218 } from './minecraft-1.21.8';
 import { minecraft1219 } from './minecraft-1.21.9';
+import { minecraft12110 } from './minecraft-1.21.10';
 
 // Export all versions as an array in correct chronological order
 export const allVersions = [
+  minecraft12110,
   minecraft1219,
   minecraft1218,
   minecraft1217,
@@ -33,6 +35,7 @@ export const allVersions = [
 
 // Export individual versions
 export {
+  minecraft12110,
   minecraft1219,
   minecraft1218,
   minecraft1217,
---
+++ src/data/versions/minecraft-1.21.10.js
@@ -0,0 +1,15 @@
+export const minecraft12110 = {
+  version: '1.21.10',
+  title: 'The Copper Age',
+  image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Minecraft_Fall_Drop_Campaign_Key_Art_DotNet_Blog_Editorial_1280x768.jpg',
+  releases: [
+    {
+      id: "170hY7Zwo",
+      version: "beta-0.1-1.0.4",
+      date: "Dec 25, 2025",
+      changelog: "Primera versión beta para Minecraft 1.21.10 Copper Age con mods básicos optimizados",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.1-1.0.4-1.21.10",
+      isBeta: true
+    }
+  ]
+};
