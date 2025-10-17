310cfe930f0c280a4a58fca08d885f1b848a84c0
SimonuwuMC <desousasimon023@gmail.com>
2025-10-17 12:05:55 -0400

    Agregar beta 0.2 para Minecraft 1.21.10

--- src/data/versions/minecraft-1.21.10.js
+++ src/data/versions/minecraft-1.21.10.js
@@ -4,6 +4,14 @@ export const minecraft12110 = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Minecraft_Fall_Drop_Campaign_Key_Art_DotNet_Blog_Editorial_1280x768.jpg',
   releases: [
     {
+      id: "180hY8Zwp",
+      version: "beta-0.2-1.0.4",
+      date: "Dec 30, 2025",
+      changelog: "Segunda versión beta para Minecraft 1.21.10 Copper Age con más mods agregados y correcciones de bugs adicionales",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.2-1.0.4-1.21.10",
+      isBeta: true
+    },
+    {
       id: "170hY7Zwo",
       version: "beta-0.1-1.0.4",
       date: "Dec 25, 2025",
