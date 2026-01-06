1be8417369f26a5c8ca1921e0c97e9952ecab6cd
SimonuwuMC <desousasimon023@gmail.com>
2026-01-06 10:49:18 -0400

    Updated minecraft-1.21.11.js

--- src/data/versions/minecraft-1.21.11.js
+++ src/data/versions/minecraft-1.21.11.js
@@ -4,10 +4,18 @@ export const minecraft12111 = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/WGD-25_Video-Banner-A_Trailer_1920x1080.jpg',
   releases: [
     {
+      id: "230hY3Zxu",
+      version: "beta-0.2-1.0.4",
+      date: "Jan 6, 2026",
+      changelog: "Segunda versión beta para Minecraft 1.21.11 Mounts of Mayhem con más mods agregados y mejoras de rendimiento",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.2-1.0.4-1.21.11",
+      isBeta: true
+    },
+    {
       id: "220hY2Zxt",
       version: "beta-0.1-1.0.4",
       date: "Dec 9, 2025",
-      changelog: "Primera versión beta para Minecraft 1.21.11 The Copper Age",
+      changelog: "Primera versión beta para Minecraft 1.21.11 Mounts of Mayhem",
       downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.1-1.0.4-1.21.11",
       isBeta: true
     }
