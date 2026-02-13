07d8682340937ecfe741d9c7ca75a6a30d06e278
SimonuwuMC <desousasimon023@gmail.com>
2026-02-13 18:35:34 -0400

    Updated minecraft-1.21.11.js

--- src/data/versions/minecraft-1.21.11.js
+++ src/data/versions/minecraft-1.21.11.js
@@ -4,6 +4,14 @@ export const minecraft12111 = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/WGD-25_Video-Banner-A_Trailer_1920x1080.jpg',
   releases: [
     {
+      id: "140hY4Zwl",
+      version: "1.0.4",
+      date: "Jul 24, 2025",
+      changelog: "Versión estable para Minecraft 1.21.8 con todos los mods actualizados y optimizados",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/1.0.4-1.21.8",
+      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project",
+      isBeta: false
+    },{
       id: "230hY3Zxu",
       version: "beta-0.2-1.0.4",
       date: "Jan 6, 2026",
