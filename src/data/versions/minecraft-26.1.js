7a4fa705d991c48b5222862faac77cf17ec28616
SimonuwuMC <desousasimon023@gmail.com>
2026-03-25 17:15:45 -0400

    Updated minecraft-26.1.js

--- src/data/versions/minecraft-26.1.js
+++ src/data/versions/minecraft-26.1.js
@@ -1,26 +1,7 @@
 export const minecraft261 = {
   version: '26.1',
-  title: 'Minecraft 26.1',
-  alternativeTitle: 'New Era',
-  image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/26.1-1170x500.jpg',
-  alternativeImage: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/26.1-launch-Header_1170x500.jpg',
-  hasImageSelector: true,
-  imageOptions: [
-    {
-      key: 'main',
-      title: 'Minecraft 26.1',
-      icon: '🎮',
-      image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/26.1-1170x500.jpg',
-      description: 'Mostrando imagen principal de Minecraft 26.1'
-    },
-    {
-      key: 'alt',
-      title: 'New Era',
-      icon: '✨',
-      image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/26.1-launch-Header_1170x500.jpg',
-      description: 'Mostrando arte alternativo de New Era'
-    }
-  ],
+  title: 'Tiny Takeover',
+  image: 'https://whatsapp.com/channel/0029VakYS0uKLaHg2yxV2T1p/1053',
   releases: [
     {
       id: "150jZ5Axm",
