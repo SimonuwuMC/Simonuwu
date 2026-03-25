7b6eecf5c6e0ec4c035e04dc380b1ea1055b56e0
SimonuwuMC <desousasimon023@gmail.com>
2026-03-25 17:11:40 -0400

    Updated minecraft-26.1.js

--- src/data/versions/minecraft-26.1.js
+++ src/data/versions/minecraft-26.1.js
@@ -23,21 +23,12 @@ export const minecraft261 = {
   ],
   releases: [
     {
-      id: "70bU5Pod",
-      version: "1.0.4",
-      date: "Mar 25, 2026",
-      changelog: "Versión estable para Minecraft 26.1 con todos los mods actualizados",
-      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/1.0.4-26.1",
-      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project",
-      isBeta: false
-    },
-    {
       id: "150jZ5Axm",
       version: "beta-0.1-1.0.4",
-      date: "Mar 20, 2026",
+      date: "Mar 25, 2026",
       changelog: "Nueva versión beta 0.1 para Minecraft 26.1 con mods básicos optimizados",
       downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.1-1.0.4-26.1",
-      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project",
+      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project/files/7813463",
       isBeta: true
     }
   ]
