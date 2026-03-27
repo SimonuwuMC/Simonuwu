abd7533a601dcebd61dda0bd78f74bc8e8f411f3
SimonuwuMC <desousasimon023@gmail.com>
2026-03-27 11:53:04 -0400

    Remove curseforgeUrl from Minecraft beta versions
    Removed curseforgeUrl from beta versions in Minecraft data.

--- src/data/versions/minecraft-26.1.js
+++ src/data/versions/minecraft-26.1.js
@@ -9,7 +9,7 @@ export const minecraft261 = {
       date: "Mar 27, 2026",
       changelog: "Nueva versión beta 0.2 para Minecraft 26.1 con mejoras para los mods básicos optimizados",
       downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.2-1.0.4-26.1",
-      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project/files/7823213",
+      curseforgeUrl: "",
       isBeta: true
     },
     {
@@ -18,7 +18,7 @@ export const minecraft261 = {
       date: "Mar 25, 2026",
       changelog: "Nueva versión beta 0.1 para Minecraft 26.1 con mods básicos optimizados",
       downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.1-1.0.4-26.1",
-      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project/files/7813463",
+      curseforgeUrl: "",
       isBeta: true
     }
   ]
