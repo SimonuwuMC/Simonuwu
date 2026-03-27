6132e93c92c6694687647c9e64afb6d61e723e9f
SimonuwuMC <desousasimon023@gmail.com>
2026-03-27 11:45:00 -0400

    Add new beta release for Minecraft 26.1

--- src/data/versions/minecraft-26.1.js
+++ src/data/versions/minecraft-26.1.js
@@ -4,6 +4,15 @@ export const minecraft261 = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Tiny%20Takeover_keyart_1170x500.jpg',
   releases: [
     {
+      id: "160jZ5Axm",
+      version: "beta-0.2-1.0.4",
+      date: "Mar 27, 2026",
+      changelog: "Nueva versión beta 0.2 para Minecraft 26.1 con mejoras para los mods básicos optimizados",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.2-1.0.4-26.1",
+      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project/files/7823213",
+      isBeta: true
+    },
+    {
       id: "150jZ5Axm",
       version: "beta-0.1-1.0.4",
       date: "Mar 25, 2026",
