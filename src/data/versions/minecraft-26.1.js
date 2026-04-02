b692f147f4281c4c7287a3bd70f735779fd9455b
SimonuwuMC <desousasimon023@gmail.com>
2026-04-01 21:48:54 -0400

    Add new beta release and update existing entries
    Updated the releases for Minecraft 26.1 to include a new beta version and modified existing entries.

--- src/data/versions/minecraft-26.1.js
+++ src/data/versions/minecraft-26.1.js
@@ -4,6 +4,15 @@ export const minecraft261 = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Tiny%20Takeover_keyart_1170x500.jpg',
   releases: [
     {
+      id: "170jZ5Axm",
+      version: "beta-0.3-1.0.4",
+      date: "Apr 1, 2026",
+      changelog: "Nueva versión beta 0.3 para Minecraft 26.1 con compatibildad con Zoom y Skins 3D",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.3-1.0.4-26.1",
+      curseforgeUrl: "",
+      isBeta: true
+    },
+    {
       id: "160jZ5Axm",
       version: "beta-0.2-1.0.4",
       date: "Mar 27, 2026",
