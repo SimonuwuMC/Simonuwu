4fb2370cb369946d709ca0a1e15116dbb4f0dea3
SimonuwuMC <desousasimon023@gmail.com>
2026-04-02 16:30:08 -0400

    Updated minecraft-26.1.1.js

--- src/data/versions/minecraft-26.1.1.js
+++ src/data/versions/minecraft-26.1.1.js
@@ -1,7 +1,7 @@
 // minecraft-26.1.1.js
 export const minecraft2611 = {
-  version: '1.21.12',
-  title: 'Nombre del Update',
+  version: '26.1.1',
+  title: 'Tiny TakeOver',
   image: 'URL de Minecraft.net',
   releases: []
 };
