171d322e691bddb64b445dd74889d367af32bdec
SimonuwuMC <desousasimon023@gmail.com>
2026-04-02 16:32:05 -0400

    Updated minecraft-26.1.1.js

--- src/data/versions/minecraft-26.1.1.js
+++ src/data/versions/minecraft-26.1.1.js
@@ -1,6 +1,6 @@
 export const minecraft2611 = {
   version: '26.1.1',
-  title: 'Tiny Takeover',
+  title: 'Tiny Takeover - Hotfix',
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Tiny%20Takeover_keyart_1170x500.jpg',
   releases: []
 };
