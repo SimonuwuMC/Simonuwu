c596ab584b40ac658512495d7030adb7364eef91
SimonuwuMC <desousasimon023@gmail.com>
2026-04-02 16:28:54 -0400

    Updated minecraft-26.1.1.js

--- src/data/versions/minecraft-26.1.1.js
+++ src/data/versions/minecraft-26.1.1.js
@@ -1,5 +1,5 @@
-// minecraft-1.21.12.js
-export const minecraft12112 = {
+// minecraft-26.1.1.js
+export const minecraft2611 = {
   version: '1.21.12',
   title: 'Nombre del Update',
   image: 'URL de Minecraft.net',
