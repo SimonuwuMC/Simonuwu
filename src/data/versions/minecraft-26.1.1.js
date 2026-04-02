9642c4a501f7ae80184be874f100c0d9150ac70e
SimonuwuMC <desousasimon023@gmail.com>
2026-04-02 16:38:08 -0400

    Updated minecraft-26.1.1.js

--- src/data/versions/minecraft-26.1.1.js
+++ src/data/versions/minecraft-26.1.1.js
@@ -2,5 +2,15 @@ export const minecraft2611 = {
   version: '26.1.1',
   title: 'Tiny Takeover - Hotfix',
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Tiny%20Takeover_keyart_1170x500.jpg',
-  releases: []
+  releases: [
+  {
+    id: "150jZ5Axm",
+      version: "beta-0.1-1.0.4",
+      date: "Mar 25, 2026",
+      changelog: "Nueva versión beta 0.1 para Minecraft 26.1 con mods básicos optimizados",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.1-1.0.4-26.1",
+      curseforgeUrl: "",
+      isBeta: true  
+   }
+  ]
 };
