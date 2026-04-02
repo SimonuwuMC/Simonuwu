3a324a9d7c6939b71b1763f67d7d48332e51643e
SimonuwuMC <desousasimon023@gmail.com>
2026-04-02 19:11:47 -0400

    Update curseforgeUrl for Minecraft version 26.1.1

--- src/data/versions/minecraft-26.1.1.js
+++ src/data/versions/minecraft-26.1.1.js
@@ -9,7 +9,7 @@ export const minecraft2611 = {
       date: "Apr 2, 2026",
       changelog: "Nueva versión beta 0.1 para Minecraft 26.1.1 con Sodium y una Api para el futuro mod ModMenu",
       downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.2-1.0.4-26.1.1",
-      curseforgeUrl: "",
+      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project/files/7864418",
       isBeta: true  
   },
   {
