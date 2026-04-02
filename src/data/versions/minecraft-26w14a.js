248ea6b0326f1fc348df3610d0c1b063e3a898be
SimonuwuMC <desousasimon023@gmail.com>
2026-04-02 17:45:41 -0400

    Add Minecraft version 26w14a with release details

---
+++ src/data/versions/minecraft-26w14a.js
@@ -0,0 +1,16 @@
+export const minecraft26w14a = {
+  version: '26w14a',
+  title: 'Herdcraft Upate April Fools 2026 ',
+  image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/2_HEADER%20_%201170%20x%20500%20copy.jpg',
+  releases: [
+  {
+    id: "150jZ5Axm",
+      version: "1-1.0.4",
+      date: "Apr 1, 2026",
+      changelog: "Nueva versión 1 para Minecraft 26w14a con mods básicos optimizados",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/1-1.0.4-26w14a",
+      curseforgeUrl: "",
+      isBeta: true  
+   }
+  ]
+};
