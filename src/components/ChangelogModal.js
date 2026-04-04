f9e90a99198bf74b3e10a954e36a233f9e509a2c
SimonuwuMC <desousasimon023@gmail.com>
2026-04-03 22:11:59 -0400

    Updated ChangelogModal.js

--- src/components/ChangelogModal.js
+++ src/components/ChangelogModal.js
@@ -8,7 +8,7 @@ const ChangelogModal = ({ isOpen, onClose }) => {
       version: "1.0.5",
       date: "3 Apr 2026",
       changes: [
-        " ## **Changes** "
+        " ## **Changes** ",
         "- Several mods have been updated to their latest available versions at this time**"
         "- **The fabric loader was updated to version 0.18.1**
         "- **Translations for Sodium was removed to avoid copyright issues. However, it can be downloaded using the Resourcify mod or manually from Modrinth or Curseforge.** 
