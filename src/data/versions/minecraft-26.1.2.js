fe128ad0f44e9754b55a0d73995005429491cf72
SimonuwuMC <desousasimon023@gmail.com>
2026-04-12 11:55:51 -0400

    Add Minecraft version 26.1.2 data

---
+++ src/data/versions/minecraft-26.1.2.js
@@ -0,0 +1,14 @@
+export const minecraft2612 = {
+  version: '26.1.2',
+  title: 'Tiny Takeover - Hotfix 2',
+  image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Tiny%20Takeover_keyart_1170x500.jpg',
+  releases: [
+  {
+    id: "160jZ5Axm",
+      version: "beta-0.1-1.0.4",
+      date: "Apr , 2026",
+      changelog: "",
+      downloadUrl: "",
+      curseforgeUrl: "",
+      isBeta: true  
+  },
