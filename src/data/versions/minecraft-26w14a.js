616a279a0a33e138f3ff9dfd53d4b55ff8015f4e
SimonuwuMC <desousasimon023@gmail.com>
2026-04-13 19:32:51 -0400

    Add new version 1-1.0.5 with changelog and URLs

--- src/data/versions/minecraft-26w14a.js
+++ src/data/versions/minecraft-26w14a.js
@@ -4,6 +4,15 @@ export const minecraft26w14a = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/2_HEADER%20_%201170%20x%20500%20copy.jpg',
   releases: [
   {
+    id: "160jZ5Axm",
+      version: "1-1.0.5",
+      date: "Apr 1, 2026",
+      changelog: "Primera version de la 1.0.5 para Minecraft 26w14a agregando Concurrent Chunk Management Engine",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/1-1.0.5-26w14a",
+      curseforgeUrl: "",
+      isBeta: true  
+   },
+    {
     id: "150jZ5Axm",
       version: "1-1.0.4",
       date: "Apr 1, 2026",
