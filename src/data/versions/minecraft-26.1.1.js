1addf0334b2bf39ff8ba65d314a09254e91b937b
SimonuwuMC <desousasimon023@gmail.com>
2026-04-18 12:12:28 -0400

    Update Minecraft version data with new beta release
    Added new beta version and updated changelog for existing version.

--- src/data/versions/minecraft-26.1.1.js
+++ src/data/versions/minecraft-26.1.1.js
@@ -4,10 +4,19 @@ export const minecraft2611 = {
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Tiny%20Takeover_keyart_1170x500.jpg',
   releases: [
   {
+    id: "170jZ5Axm",
+      version: "beta-0.3-1.0.4",
+      date: "Apr 18, 2026",
+      changelog: "Nueva versión beta 0.3 para Minecraft 26.1.1 con cambios de provedor y la mayoría de mods",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.3-1.0.4-26.1.1",
+      curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project/files/7945762",
+      isBeta: true  
+  },
+  {
     id: "160jZ5Axm",
       version: "beta-0.2-1.0.4",
       date: "Apr 2, 2026",
-      changelog: "Nueva versión beta 0.1 para Minecraft 26.1.1 con Sodium y una Api para el futuro mod ModMenu",
+      changelog: "Nueva versión beta 0.2 para Minecraft 26.1.1 con Sodium y una Api para el futuro mod ModMenu",
       downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.2-1.0.4-26.1.1",
       curseforgeUrl: "https://www.curseforge.com/minecraft/modpacks/simonuwu-fabric-project/files/7864418",
       isBeta: true  
