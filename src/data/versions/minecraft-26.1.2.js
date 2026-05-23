11584d80209f28625df5c6be1f81423208491656
SimonuwuMC <desousasimon023@gmail.com>
2026-05-23 15:04:49 -0400

    Add new beta release for Minecraft 26.1.2

--- src/data/versions/minecraft-26.1.2.js
+++ src/data/versions/minecraft-26.1.2.js
@@ -3,7 +3,16 @@ export const minecraft2612 = {
   title: 'Tiny Takeover - Hotfix 2',
   image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/key-art/Tiny%20Takeover_keyart_1170x500.jpg',
   releases: [
-  {
+    {
+    id: "170jZ5Axm",
+      version: "beta-0.2-1.0.4",
+      date: "May 23, 2026",
+      changelog: "Nueva versión beta 0.2 volviendo de vacaciones para Minecraft 26.1 con mods básicos agregados",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.2-1.0.4-26.1.2",
+      curseforgeUrl: "",
+      isBeta: true  
+    },
+    {
     id: "160jZ5Axm",
       version: "beta-0.1-1.0.4",
       date: "Apr 12, 2026",
