153c70957b47793439b19629cd9a73559b082366
SimonuwuMC <desousasimon023@gmail.com>
2026-08-11 10:54:13 -0400

    Update Minecraft version details in versions file

--- src/data/versions/minecraft-26.2.js
+++ src/data/versions/minecraft-26.2.js
@@ -5,12 +5,12 @@ export const minecraft262 = {
   releases: [
     {
     id: "160jZ5Axm",
-      version: "beta-0.1-1.0.4",
-      date: "Aug 0, 2026",
-      changelog: "Nueva versión beta 0.1 para Minecraft 26.2 con mods básicos optimizados",
-      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.1-1.0.4-26.2",
+      version: "1.0.4",
+      date: "Aug 11, 2026",
+      changelog: "Nueva versión para Minecraft 26.2 con todos los mods optimizados",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/1.0.4-26.2",
       curseforgeUrl: "",
-      isBeta: true  
+      isBeta: false  
     }
   ]
 };
