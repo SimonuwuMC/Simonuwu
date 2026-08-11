bc7bede15998f53d2bae56c47037a487d15cffd9
SimonuwuMC <desousasimon023@gmail.com>
2026-08-11 10:25:05 -0400

    Add Minecraft version 26.2 data
    Added new version data for Minecraft 26.2 including title, image, and release details.

---
+++ src/data/versions/minecraft-26.2.js
@@ -0,0 +1,16 @@
+export const minecraft262 = {
+  version: '26.2',
+  title: 'Chaos Cubed',
+  image: 'https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/MCV_SummerDropKeyArt_NetBlogEditorial_1170x500.jpg',
+  releases: [
+    {
+    id: "160jZ5Axm",
+      version: "beta-0.1-1.0.4",
+      date: "Aug 0, 2026",
+      changelog: "Nueva versión beta 0.1 para Minecraft 26.2 con mods básicos optimizados",
+      downloadUrl: "https://modrinth.com/modpack/simonuwu-fabric-project/version/0.1-1.0.4-26.2",
+      curseforgeUrl: "",
+      isBeta: true  
+    }
+  ]
+};
